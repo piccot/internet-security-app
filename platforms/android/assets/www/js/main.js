f2d37eabe0f273015dc8cb7842e07c2564df0347
piccot <piccot@live.unc.edu>
2016-01-27 17:52:40 -0500

    Update Layout
    Updated Layout to add image and various color changes

--- platforms/android/assets/www/js/main.js
+++ platforms/android/assets/www/js/main.js
@@ -1,24 +1,29 @@
 function createCards(){
 	var container = document.getElementsByClassName("scroll-holder")[0]
 	var width = Math.floor(container.offsetWidth * .75)
-	var height = Math.floor(width * (4/3)) * 1.2
+	var height = Math.floor(width * (4/3)) * 1.15
 	for (i=0;i<6;i++){
 		var div = document.createElement("div");
-		var innerDiv = document.createElement("div");
-		innerDiv.style.width = '100%'
-		innerDiv.style.height = '15%'
-		innerDiv.style.position = 'relative';
-		innerDiv.style.bottom = '-85%';
-		innerDiv.style.backgroundColor = 'green'
-		div.appendChild(innerDiv);
+	
+		var upperImg = document.createElement("img");
+		upperImg.style.width= '100%'
+		upperImg.style.height = '85%'
+		upperImg.src = 'splashscreens/whack.png'
+		div.appendChild(upperImg)
+		var lowerDiv = document.createElement("div");
+		lowerDiv.style.width = '100%'
+		lowerDiv.style.height = '15%'
+		lowerDiv.style.position = 'relative';		
+		lowerDiv.style.backgroundColor = 'green'
+		div.appendChild(lowerDiv);
 		
 		div.style.width= width + 'px';
 		div.style.height= height +'px';
-		div.style.backgroundColor = 'blue';
+		div.style.backgroundColor = 'black';
 		div.style.position = 'relative';
 		div.style.display = 'inline-block';
 		div.style.top = (container.offsetHeight - height)/2 + 'px'
-		div.style.left = width * .075 *i + 'px'
+		div.style.left = width * .075 *(i+1) + 'px'
 		container.appendChild(div)
 	
 	}
--- www/js/main.js
+++ www/js/main.js
@@ -1,22 +1,21 @@
 function createCards(){
 	var container = document.getElementsByClassName("scroll-holder")[0]
 	var width = Math.floor(container.offsetWidth * .75)
-	var height = Math.floor(width * (4/3)) * 1.2
+	var height = Math.floor(width * (4/3)) * 1.15
 	for (i=0;i<6;i++){
 		var div = document.createElement("div");
-		var innerDiv = document.createElement("div");
-		innerDiv.style.width = '100%'
-		innerDiv.style.height = '15%'
-		innerDiv.style.position = 'relative';
-		innerDiv.style.bottom = '-85%';
-		innerDiv.style.backgroundColor = 'green'
-		div.appendChild(innerDiv);
+	
+		var upperImg = document.createElement("img");		
+		upperImg.src = 'splashscreens/whack.png'
+		div.appendChild(upperImg)
+		
+		
+		var lowerDiv = document.createElement("div");		
+		div.appendChild(lowerDiv);
 		
 		div.style.width= width + 'px';
-		div.style.height= height +'px';
-		div.style.backgroundColor = 'blue';
-		div.style.position = 'relative';
-		div.style.display = 'inline-block';
+		div.style.height= height +'px';		
+		div.className = 'gameCard'		
 		div.style.top = (container.offsetHeight - height)/2 + 'px'
 		div.style.left = width * .075 *(i+1) + 'px'
 		container.appendChild(div)
