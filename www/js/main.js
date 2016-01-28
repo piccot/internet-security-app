385d147b485e6e217a3d31b23e806a894b4f2c32
piccot <piccot@live.unc.edu>
2016-01-28 09:38:10 -0500

    Add Play Button
    Added play button and click function to game cards

--- www/js/main.js
+++ www/js/main.js
@@ -1,27 +1,31 @@
-function createCards(){
+function createCard(splash,home,i){
 	var container = document.getElementsByClassName("scroll-holder")[0]
 	var width = Math.floor(container.offsetWidth * .75)
 	var height = Math.floor(width * (4/3)) * 1.15
-	for (i=0;i<6;i++){
-		var div = document.createElement("div");
+	var div = document.createElement("div");
+
+	var upperImg = document.createElement("img");		
+	upperImg.src = 'splashscreens/'+splash
+	div.appendChild(upperImg)
 	
-		var upperImg = document.createElement("img");		
-		upperImg.src = 'splashscreens/whack.png'
-		div.appendChild(upperImg)
-		
-		
-		var lowerDiv = document.createElement("div");		
-		div.appendChild(lowerDiv);
-		
-		div.style.width= width + 'px';
-		div.style.height= height +'px';		
-		div.className = 'gameCard'		
-		div.style.top = (container.offsetHeight - height)/2 + 'px'
-		div.style.left = width * .075 *(i+1) + 'px'
-		container.appendChild(div)
 	
-	}
+	var lowerImg = document.createElement("img");
+	lowerImg.src = 'assets/img/playbutton.png'
+	lowerImg.onclick = function(){window.location.href = home};
+	div.appendChild(lowerImg);
+	
+	div.style.width= width + 'px';
+	div.style.height= height +'px';		
+	div.className = 'gameCard'		
+	div.style.top = (container.offsetHeight - height)/2 + 'px'
+	div.style.left = width * .075 *(i+1) + 'px'
+	container.appendChild(div)
+	
+	
 	
 }
 
-createCards();
+createCard('whack.png','whack.html',0);
+createCard('whack.png','whack.html',1);
+createCard('whack.png','whack.html',2);
+createCard('whack.png','whack.html',3);
