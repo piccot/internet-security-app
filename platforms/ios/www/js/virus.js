054aee21c9e95c954a31443915f784c1c8d8ba74
mattheh <mattheh@email.unc.edu>
2016-02-16 16:24:57 -0500

    Whack: Update Scoring; Mail: Add Mail image

--- platforms/ios/www/js/virus.js
+++ platforms/ios/www/js/virus.js
@@ -5,6 +5,13 @@ window.onerror = function(msg, url, linenumber) {
 var canvas;
 var ctx;
 var bucket;
+var av_button;
+var av_counter = 0;
+var base_penalty = 5000;
+var av_speed_mod = .1;
+var av_size_mod = -.1;
+var win_score = 64;
+var totalTime = 0;
 var app = {
 
   
@@ -32,6 +39,9 @@ var app = {
 		lastTime = Date.now()
 		main();
 		bucket = new bucket();
+		av_button = new av_button();
+		ctx.font = "24pt Ariel"
+        ctx.textAlign="left";
     },
 
 };
@@ -42,22 +52,30 @@ var virus_green_image = new Image();
 virus_green_image.src = 'assets/img/virus_green.png';
 var data_bucket_image = new Image();
 data_bucket_image.src = 'assets/img/data_bucket.png';
+var av_arr  = [];
+for(i = 0; i < 6; i ++){
+	var av_count_image = new Image();
+	av_count_image.src = 'assets/img/av_count_' + i + '.png';
+	av_arr.push(av_count_image);
+
+}
 var hitSound = new Audio("assets/audio/hit.wav")
 var missSound = new Audio("assets/audio/miss.wav")
+var notificationSound = new Audio("assets/audio/notification.wav")
 var virus_arr = [];
-function virus(x,y,dx,dy,id,type){
+function virus(x,y,dx,dy,id,type,mod){
 	this.x = x;
 	this.y=y;
 	this.id = id;
 	this.type = type
-    this.width = window.innerWidth/20;
+    this.width = (window.innerWidth/20) * (Math.pow(1+av_size_mod,mod));
     this.height = this.width;
 	if (type == 1 )
 		this.img = virus_green_image
 	else 
 		this.img = virus_red_image
-	this.dx = dx
-	this.dy = dy
+	this.dx = dx * (Math.pow(1+av_speed_mod,mod))
+	this.dy = dy * (Math.pow(1+av_speed_mod,mod))
 }
 function bucket(){
 	this.x = canvas.width - canvas.height * .2;
@@ -65,9 +83,15 @@ function bucket(){
 	this.size = canvas.height * .2;
 
 }
+function av_button(){
+	this.x = canvas.width - canvas.height * .11;
+	this.y = canvas.height*.89;
+	this.size = canvas.height * .1;
+
+}
 var lastTime = Date.now();
 function main (){
-	if (score == 5){
+	if (score == win_score){
 		alert('win');
 		score = 0;
 	}
@@ -89,21 +113,34 @@ function render(){
     
 	}
 	ctx.drawImage(data_bucket_image,bucket.x,bucket.y,bucket.size,bucket.size)
-	 if (held)
+	ctx.drawImage(av_arr[av_counter],av_button.x,av_button.y,av_button.size,av_button.size)
+	ctx.strokeText((Math.floor(totalTime/10)/100).toFixed(2),10,45);
+    ctx.fillText((Math.floor(totalTime/10)/100).toFixed(2),10,45);
+	if (held)
 		ctx.drawImage(held.img,held.x,held.y,held.width,held.height)
 }
 var currentID = 0;
 var millisecondsPerVirus = 1000;
 var baseSpeed = 4000;
+var millisecondsPerUpdate = 10000;
 function editObjects(dt){
+	totalTime = totalTime + dt;
 	for(var i = 0; i < virus_arr.length; i++){
 		var current = virus_arr[i];
 		current.x = current.x + current.dx * dt;
 		current.y = current.y + current.dy * dt;
-		if (current.x > window.innerWidth || current.x < 0 || current.y > window.innerHeight || current.y < 0){
+		if (current.x > window.innerWidth || current.x + current.width < 0 || current.y > window.innerHeight || current.y + current.height < 0){
+			if (current.type == 2){
+				missSound.play()
+				totalTime = totalTime + base_penalty * av_counter;
+			}
 			virus_arr.splice(i,1);
 		}
 		
+	}
+	if (Math.random() < (1/millisecondsPerUpdate) * dt && av_counter < 5){
+		av_counter++;
+		notificationSound.play();
 	}
 	if (Math.random() < (1/millisecondsPerVirus)*dt){
 		var rnd = getRandomInt(0,3);
@@ -148,13 +185,16 @@ function editObjects(dt){
 				deltaX = window.innerWidth/baseSpeed;
 		
 		}
-		var rnd2 = getRandomInt(0,10)
+		var rnd2 = getRandomInt(0,20)
 		var virus_type = 1
-		if(rnd2 == 10)
+		console.log(rnd2 + ' ' + (19-av_counter))
+		if(rnd2 >= (19 - av_counter)){
 			virus_type = 2
-		
-		console.log(xcoord + ' ' + ycoord + ' ' + deltaX + ' ' + deltaY)
-		virus_arr.push(new virus(xcoord,ycoord,deltaX,deltaY,currentID,virus_type))
+			virus_arr.push(new virus(xcoord,ycoord,deltaX,deltaY,currentID,virus_type,av_counter))
+		} else 
+		{
+			virus_arr.push(new virus(xcoord,ycoord,deltaX,deltaY,currentID,virus_type,0))
+		}
 		currentID++;
 	
 	}
@@ -172,6 +212,9 @@ function touchStart(e){
 					return true;
 				}
 			}
+			if (e.touches[i].pageX >= av_button.x && e.touches[i].pageX <= av_button.x +av_button.size && e.touches[i].pageY >= av_button.y && e.touches[i].pageY <= av_button.y + av_button.size){
+				av_counter = 0;
+			}
 		}
 }
 function touchEnd(e){
@@ -179,8 +222,7 @@ function touchEnd(e){
 		
 		if (held.x + held.width/2 >= bucket.x && held.x + held.width/2 <= bucket.x + bucket.size && held.y >= bucket.y && held.y <= bucket.y + bucket.size){
 			score ++;
-			hitSound.play()
-		
+			hitSound.play();
 		}
 		else
 			virus_arr.push(held);
